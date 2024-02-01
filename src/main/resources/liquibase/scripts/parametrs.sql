e5b86cdaec5bce20f67a90729eb5251ce5cebbb7
kiruha3 <kiruha3@mail.ru>
2024-02-01 20:03:21 +0700

    сущность NotificationTask добавлена

--- src/main/resources/liquibase/scripts/parametrs.sql
+++ src/main/resources/liquibase/scripts/parametrs.sql
@@ -1,1 +1,10 @@
 -- liquibase formatted sql
+
+-- changeset kiruha3:1
+CREATE TABLE notification_task
+(
+    id        bigserial primary key,
+    chat_id   bigserial,
+    text      varchar,
+    exec_date timestamp
+);
